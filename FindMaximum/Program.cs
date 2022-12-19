4109a692207caede2bd07fe31fab3cde95a154ee
unknown <abhimvj575@gmail.com>
2022-12-19 21:20:30 +0530

    [Abhilash] Added: Finding Maximum of Three Numbers

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FindMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float max = MaxFloat.FindMax(20.50f, 40.60f, 60.70f);
+            Console.WriteLine(max);
+        }
+    }
+}
